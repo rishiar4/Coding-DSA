26f03a9c92c9e3c65d6b8b04179915828f1b42cb
unknown <rishabh@appfoster.com>
2023-09-07 19:54:21 +0530

    recursion updates

--- Recursion/l001.java
+++ Recursion/l001.java
@@ -1,26 +1,56 @@
 public class l001 {
 
     public static void printIncreasing(int num1, int num2) {
-        if(num1 == num2 + 1) {
+        if (num1 == num2 + 1) {
             return;
         }
 
-        System.out.print(num1 + " ");
-        printIncreasing(num1+1, num2);
+        System.out.println(num1);
+
+        printIncreasing(num1 + 1, num2);
     }
 
     public static void printDecreasing(int num1, int num2) {
-        if(num1 == num2 + 1) {
+        if (num1 == num2 + 1) {
             return;
         }
 
-        printDecreasing(num1+1, num2);
-        System.out.print(num1 + " ");
+        printDecreasing(num1 + 1, num2);
+
+        System.out.println(num1);
+    }
+
+    public static void printOddEven(int num1, int num2) {
+        if (num1 == num2 + 1) {
+            return;
+        }
+
+        if (num1 % 2 == 0) {
+            System.out.println(num1);
+        }
+
+        printOddEven(num1 + 1, num2);
+
+        if (num1 % 2 != 0) {
+            System.out.println(num1);
+        }
+
+    }
+
+    public static int fact(int num1) {
+        return num1 == 0 ? 1 : fact(num1 - 1) * num1;
+    }
+
+    public static int power(int a, int b) {
+        return b == 1 ? 1 : power(a, b - 1) * a;
     }
 
     public static void main(String[] args) {
         int num1 = 1, num2 = 10;
         // printIncreasing(num1, num2);
-        printDecreasing(num1, num2);
+        // printDecreasing(num1, num2);
+        // printOddEven(num1, num2);
+        // System.out.println(fact(num2));
+        System.out.println(power(10, 5));
     }
 }
